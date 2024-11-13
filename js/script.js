ce72d91b597696dc2bf366d2e795be0541477044
Ramen Biswas <printrb72@gmail.com>
2024-11-13 08:03:12 +0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const playerInput1 = document.querySelector("#playerInput1");
+const playerInputBtn1 = document.querySelector("#playerInputBtn1");
+const errorMessage = document.querySelector("#errorMessage");
+
+const playerInput2 = document.querySelector("#playerInput2");
+const playerInputBtn2 = document.querySelector("#playerInputBtn2");
+
+playerInputBtn1.addEventListener("click",()=>{
+    console.log(playerInput1.value);
+    
+})
+
+
